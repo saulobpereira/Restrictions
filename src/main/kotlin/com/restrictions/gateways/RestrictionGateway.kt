02e47aaeba04d3d47859058276fe4349d4afcc46
Saulo Pereira <saulobp@gmail.com>
2018-01-02 12:44:34 -0200

    Adicionando método

--- src/main/kotlin/com/restrictions/gateways/RestrictionGateway.kt
+++ src/main/kotlin/com/restrictions/gateways/RestrictionGateway.kt
@@ -4,4 +4,5 @@ import com.restrictions.entities.Restriction
 
 interface RestrictionGateway {
     fun getRestriction(code: String) : Restriction
+    fun getProductRestrictions(productCode: String): List<Restriction>
 }
